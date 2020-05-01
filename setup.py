c293eaebd9e7fb5a4a8a597a0414fe7274f25f79
jessebrennan <brennan@ucsc.edu>
2020-05-01 10:56:50 -0700

    Version 1.14.0+3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='chalice',
-    version='1.18.0',
+    version='1.14.0+3',
     description="Microframework",
     long_description=README,
     author="James Saryerwinnie",

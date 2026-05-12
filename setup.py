27dcd8cdd70c799e639507d131cdb3f0c22e046c
Abraham Chavez <achave11@ucsc.edu>
2026-05-12 13:47:23 -0700

    Version 1.32.0+21

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
 
 setup(
     name='chalice',
-    version='1.32.0+20',
+    version='1.32.0+21',
     description="Microframework",
     long_description=README,
     author="James Saryerwinnie",
